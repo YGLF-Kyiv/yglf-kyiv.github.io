f1c19253c201330fbd7f80c4569f3b773a3b5326
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-10-31 23:26:15 +0200

    Added simple layout for invitations

---
+++ path----131f9aef94525eeb6ecd.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{"./node_modules/json-loader/index.js!./.cache/json/layout-index.json":function(o,n){o.exports={layoutContext:{}}},"./node_modules/json-loader/index.js!./.cache/json/layout-simple.json":function(o,n){o.exports={layoutContext:{}}}});
+//# sourceMappingURL=path----131f9aef94525eeb6ecd.js.map
